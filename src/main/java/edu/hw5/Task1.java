26766428f39ff357d53b2f803f0afc05dc929985
W4NTER <snegovikvadim@gmail.com>
2023-11-12 02:59:41 +0500

    checkstule_wrongs

--- src/main/java/edu/hw5/Task1.java
+++ src/main/java/edu/hw5/Task1.java
@@ -3,12 +3,13 @@ package edu.hw5;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Date;
 
 public final class Task1 {
     private Task1() {
     }
+
+    private final static int SECONDS_IN_HOUR = 3600;
+    private final static int SECONDS_IN_MINUTE = 60;
 
     public static long timeInClub(String input) throws ParseException {
         String[] strStartEnd = input.split(" - ");
@@ -23,14 +24,15 @@ public final class Task1 {
         for (String time : input) {
             fullTimeInClub += timeInClub(time);
         }
-        long averageTimeInClub = fullTimeInClub / input.length; //Может получиться неровное число, переделать на даблы, сделать округление
-        long hours = averageTimeInClub / 3600;
-        long min = (averageTimeInClub % 3600) / 60 ;
-        return String.format("%dч %dм",hours, min);
+        long averageTimeInClub = fullTimeInClub / input.length;
+        //Может получиться неровное число, переделать на даблы, сделать округление
+        long hours = averageTimeInClub / SECONDS_IN_HOUR;
+        long min = (averageTimeInClub % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE;
+        return String.format("%dч %dм", hours, min);
     }
 
-    public static void main(String[] args) throws ParseException {
-        String[] str = {"2022-04-01, 21:30 - 2022-04-02, 01:20", "2022-03-12, 20:20 - 2022-03-12, 23:50"};
-        System.out.println((averageTimeInTheClub(str)));
-    }
+//    public static void main(String[] args) throws ParseException {
+//        String[] str = {"2022-04-01, 21:30 - 2022-04-02, 01:20", "2022-03-12, 20:20 - 2022-03-12, 23:50"};
+//        System.out.println((averageTimeInTheClub(str)));
+//    }
 }
