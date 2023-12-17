aa9dc358001c450d0d2fe46f19435d27b9082bdf
W4NTER <snegovikvadim@gmail.com>
2023-12-18 00:48:56 +0500

    smfg

---
+++ src/main/java/edu/hw8/Task2/FixedThreadPool.java
@@ -0,0 +1,22 @@
+package edu.hw8.Task2;
+
+import java.util.concurrent.Executors;
+
+public class FixedThreadPool implements ThreadPool{
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void execute(Runnable runnable) {
+
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+
+    var a = Executors.newFixedThreadPool()
+}
