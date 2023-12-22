9248699472529ecf08cc32de15991d083c47cf71
W4NTER <snegovikvadim@gmail.com>
2023-12-23 01:58:30 +0500

    last_version_of_2nd_task

--- src/main/java/edu/hw8/Task2/Fibonacci.java
+++ src/main/java/edu/hw8/Task2/Fibonacci.java
@@ -2,10 +2,13 @@ package edu.hw8.Task2;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public final class Fibonacci {
     private static final Logger LOGGER = LogManager.getLogger();
-    private static long result;
     private final static int START_FIB = 1;
 
     private Fibonacci() {
@@ -28,47 +31,18 @@ public final class Fibonacci {
         return fib;
     }
 
-    public static long calc(long n) {
-        if (n <= 1) {
-            return n;
-        }
-        return calc(n - 1) + calc(n - 2);
-    }
-
     public static long getFibValue(int countThreads, long countValues) {
-        long time = System.currentTimeMillis();
-        long r = -1;
+        long result = -1;
         try {
             FixedThreadPool threadPool = FixedThreadPool.create(countThreads);
             threadPool.start();
-            threadPool.execute(() -> {
-                result = calc(countValues);
-            });
+
+            result = CompletableFuture.supplyAsync(() -> calcFibonacci(countValues), threadPool).get();
+
             threadPool.close();
         } catch (Exception e) {
             LOGGER.info(e.getMessage());
         }
         return result;
     }
-
-//    public static long getVal(int countThreads, long countValues) throws ExecutionException, InterruptedException {
-//        long time = System.currentTimeMillis();
-//        ExecutorService service = Executors.newFixedThreadPool(countThreads);
-////        Future<Long> res = service.submit(() -> calc(countValues));
-//
-//        var a = CompletableFuture.supplyAsync(() -> calc(countValues), service);
-//
-//        System.out.println(System.currentTimeMillis() - time);
-//        return a.get();
-//    }
-
-//    public static void main(String[] args) throws ExecutionException, InterruptedException {
-//        long time = System.currentTimeMillis();
-//        System.out.println("single " + calc(30));
-//        System.out.println("single " + (System.currentTimeMillis() - time));
-//
-//        System.out.println("multi " + getFibValue(10, 30));
-//        System.out.println();
-//        System.out.println(getVal(4, 30));
-//    }
 }
--- src/test/java/edu/hw8/Task2Test.java
+++ src/test/java/edu/hw8/Task2Test.java
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task2Test {
-//    @Test
-//    @DisplayName("Проверка правильности вычисления числа Фибаначчи")
-//    void testThatCalculateFibonacciByMultiThreadsReturnedSucceed() {
-//        final int COUNT_VALUES = 83;
-//        final int COUNT_THREADS = 4;
-//
-//
-//        final long RESULT = Fibonacci.getFibValue(COUNT_THREADS, COUNT_VALUES);
-//
-//        final long EXPECTED_VALUE = 99194853094755497L;
-//        assertEquals(EXPECTED_VALUE, RESULT);
-//    }
+    @Test
+    @DisplayName("Проверка правильности вычисления числа Фибаначчи")
+    void testThatCalculateFibonacciByMultiThreadsReturnedSucceed() {
+        final int COUNT_VALUES = 83;
+        final int COUNT_THREADS = 4;
+
+
+        final long RESULT = Fibonacci.getFibValue(COUNT_THREADS, COUNT_VALUES);
+
+        final long EXPECTED_VALUE = 99194853094755497L;
+        assertEquals(EXPECTED_VALUE, RESULT);
+    }
 }
